642e07dcdf851eb62570f617db8bd3ba67cfd8d8
Mraz Tevin <millatevin@gmail.com>
2022-01-20 10:47:22 +0300

    delete code and create password object

--- RunEmail.java
+++ RunEmail.java
@@ -3,31 +3,11 @@ public class RunEmail {
 
     
     public static void main(String args[]) {
-        //System.out.println("New Worker: " + firstname + ". Department code\n1 for Sales\n2 for Development\n3 for accounting\n0 if None \n Enter department codes: ");
-        //Scanner choice = new Scanner(System.in);
-        
-       // EmailDetails user1 = new EmailDetails();
-        //user1.getEmailDetails();    
-        
-       // EmailDetails user = new EmailDetails();
-        
-       // String firstname = sc.next();
-       // System.out.println("Enter your first name");
-    
-       // String lastname = sc.next();
-       // System.out.println("Enter your last name");
-
-        // String department = sc.next();
-        // System.out.println("Enter your department");
-        
-       //  user.set(firstname);
-       //  user.set(lastname);
-        // user.seet(department);
-       
-        //System.out.println(user);
 
         EmailDetails user = new EmailDetails();
         user.getEmailDetails();
-          
+       
+        EmailPassword pass = new EmailPassword();
+        pass.printPassword();   
     }
 }
