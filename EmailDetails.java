0d57558f7c7e716a2d713927dad2f15d37ba8ae2
Mraz Tevin <millatevin@gmail.com>
2022-01-20 10:09:40 +0300

    modified objects and enabled user input

--- EmailDetails.java
+++ EmailDetails.java
@@ -1,13 +1,30 @@
+import java.util.Scanner;
 public class EmailDetails {
-   // public int defaultPasswordLength = 8;
+
+    public EmailDetails() {};
+
     public void getEmailDetails() {
         String email;
-        Email email1 = new Email("Ian", "Smith", "Sales");
-        EmailAddress address = new EmailAddress();
-        email = address.createEmailAdress(email1); 
-        System.out.println(email);
 
-        EmailPassword password = new EmailPassword();
-        password.printPassword();
+        EmailAddress address = new EmailAddress();
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your first name: ");
+        String first = sc.next();
+        
+        System.out.println("Enter your last name: ");
+        String last = sc.next();
+        
+        System.out.println("Enter your department: ");
+        String dept = sc.next();
+
+        Email email1 = new Email(first,last,dept);
+        email1.setFname(first);
+        email1.setLname(last);
+        email1.setDepartment(dept);
+
+        email = address.createEmailAdress(email1);
+        System.out.println(email);
     }
 }
